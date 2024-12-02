b5aedbf54f2755f59a3d01d63965c1548e680508
Kassma <doerfer.cedric@gmail.com>
2024-12-02 19:45:54 +0100

    added IInputReader interface

---
+++ AdventOfCode2024/Day01/Task01/InputReader/IInputReader.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2024.Day01.Task01.InputReader;
+
+using System.Collections.Generic;
+
+public interface IInputReader
+{
+    public static abstract IEnumerable<(int NumOne, int NumTwo)> ReadInputString(string input);
+}
