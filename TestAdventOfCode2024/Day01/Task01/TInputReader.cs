064b049ae5266c30a007a93e21dea510211269ff
Kassma <doerfer.cedric@gmail.com>
2024-12-02 20:41:12 +0100

    added Unit-Tests for "InputReader"

---
+++ TestAdventOfCode2024/Day01/Task01/TInputReader.cs
@@ -0,0 +1,23 @@
+﻿namespace TestAdventOfCode2024.Day01.Task01;
+
+using System.Linq;
+using AdventOfCode2024.Day01.Task01.InputReader;
+
+[TestFixture]
+public class TInputReader
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ValidInputStrings))]
+    public (int NumOne, int NumTwo)[]
+        ReadInputString_ValidInputString_ShouldReturnNumPairs(string inputString)
+    {
+        // assert
+        return InputReader.ReadInputString(inputString).ToArray();
+    }
+
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.InvalidInputStrings))]
+    public void ReadInputString_InvalidInputStrings_ShouldThrowArgumentexception(string inputString)
+    {
+        // assert
+        Assert.Throws<ArgumentException>(() => InputReader.ReadInputString(inputString).ToArray());
+    }
+}
