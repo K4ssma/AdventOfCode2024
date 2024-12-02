a447540f7d5f2f0d8fd1fa58c5c690ab5ebc7d8b
Kassma <doerfer.cedric@gmail.com>
2024-12-02 22:02:59 +0100

    implemented funtionality for "DistanceCalculator" class

--- AdventOfCode2024/Day01/Task01/DistanceCalculator.cs
+++ AdventOfCode2024/Day01/Task01/DistanceCalculator.cs
@@ -6,6 +6,23 @@ public static class DistanceCalculator
 {
     public static int CalculateOverallDistance((int NumOne, int NumTwo)[] numPairs)
     {
-        throw new NotImplementedException();
+        int[] firstNums = numPairs
+            .Select(numPair => numPair.NumOne)
+            .Order()
+            .ToArray();
+
+        int[] secondNums = numPairs
+            .Select(numPair => numPair.NumTwo)
+            .Order()
+            .ToArray();
+
+        int[] distances = new int[firstNums.Length];
+
+        for (int i = 0; i < distances.Length; i++)
+        {
+            distances[i] = Math.Abs(firstNums[i] - secondNums[i]);
+        }
+
+        return distances.Aggregate((curr, next) => curr + next);
     }
 }
