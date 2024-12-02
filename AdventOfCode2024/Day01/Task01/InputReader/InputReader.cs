1623bd4e3fd63b2dd9b253fb4ebc664a222b857e
Kassma <doerfer.cedric@gmail.com>
2024-12-02 19:52:38 +0100

    added "InputReader" class without functionality

---
+++ AdventOfCode2024/Day01/Task01/InputReader/InputReader.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2024.Day01.Task01.InputReader;
+
+using System;
+using System.Collections.Generic;
+
+public class InputReader : IInputReader
+{
+    public static IEnumerable<(int NumOne, int NumTwo)> ReadInputString(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
