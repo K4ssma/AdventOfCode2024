e3316c3088ea74ba662c11a66fe9202cede6b484
Kassma <doerfer.cedric@gmail.com>
2024-12-02 21:34:00 +0100

    added Unit-Tests for "DistanceCalculator"

---
+++ TestAdventOfCode2024/Day01/Task01/TDistanceCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day01.Task01;
+
+using AdventOfCode2024.Day01.Task01;
+
+[TestFixture]
+public static class TDistanceCalculator
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.DistanceCases))]
+    public static int CalculateOverallDistance_ShouldReturnCorrectDistance((int NumOne, int NumTwo)[] numbers)
+    {
+        // assert
+        return DistanceCalculator.CalculateOverallDistance(numbers);
+    }
+}
