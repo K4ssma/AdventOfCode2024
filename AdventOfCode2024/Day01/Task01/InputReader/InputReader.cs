d9a9d597834788700ddfadcb39d0a1e252aa47af
Kassma <doerfer.cedric@gmail.com>
2024-12-02 21:15:24 +0100

    implemented functionality for "InputReader.ReadInputString"

--- AdventOfCode2024/Day01/Task01/InputReader/InputReader.cs
+++ AdventOfCode2024/Day01/Task01/InputReader/InputReader.cs
@@ -7,6 +7,28 @@ public class InputReader : IInputReader
 {
     public static IEnumerable<(int NumOne, int NumTwo)> ReadInputString(string input)
     {
-        throw new NotImplementedException();
+        string[] numStringPairs = input.Trim().Split("\r\n");
+
+        return numStringPairs
+            .Select(numStringPair =>
+            {
+                string[] numStrings = numStringPair.Trim().Split(' ');
+
+                numStrings = numStrings
+                    .Where(numString => numString != string.Empty)
+                    .ToArray();
+
+                if (numStrings.Length != 2)
+                {
+                    throw new ArgumentException($"input string has wrong format near {numStringPair}" +
+                        $", numbers need to be grouped by two but was group of {numStrings.Length}");
+                }
+
+                return int.TryParse(numStrings[0], out int numOne)
+                    && int.TryParse(numStrings[1], out int numTwo)
+                    ? (numOne, numTwo)
+                    : throw new ArgumentException($"input string has wrong format near {numStringPair}" +
+                        ", numbers are not parseable to an integer");
+            });
     }
 }
