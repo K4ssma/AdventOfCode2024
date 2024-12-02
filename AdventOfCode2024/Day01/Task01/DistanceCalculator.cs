663e5528754033f620ecdf5dd303451fedfbcaae
Kassma <doerfer.cedric@gmail.com>
2024-12-02 21:25:15 +0100

    added "DistanceCalculator" class without functionality

---
+++ AdventOfCode2024/Day01/Task01/DistanceCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day01.Task01;
+
+using System;
+
+public static class DistanceCalculator
+{
+    public static int CalculateOverallDistance((int NumOne, int NumTwo)[] numPairs)
+    {
+        throw new NotImplementedException();
+    }
+}
