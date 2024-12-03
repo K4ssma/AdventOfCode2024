4b4fc00e3a47824cf7e228c63fc46da05e1b15a7
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 10:00:13 +0100

    added "InputReader" class without functionality

---
+++ AdventOfCode2024/Day02/InputReader/InputReader.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2024.Day02.InputReader;
+
+using System;
+using System.Collections.Generic;
+
+public class InputReader : IInputReader
+{
+    public static IEnumerable<IEnumerable<int>> ReadInputString(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
