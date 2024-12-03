596d2b43f14776e2b7fbbf5a654597c362443dcb
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 10:13:27 +0100

    added Unit-Test for "Day02.InputReader"

---
+++ TestAdventOfCode2024/Day02/TInputReader.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAdventOfCode2024.Day02;
+
+using System.Linq;
+using AdventOfCode2024.Day02.InputReader;
+
+[TestFixture]
+public sealed class TInputReader
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.InputCases))]
+    public int[][] ReadInputString_ValidInputs_ShouldReturnCorrectInputRepresentation(string input)
+    {
+        // assert
+        return InputReader.ReadInputString(input).Select(report => report.ToArray()).ToArray();
+    }
+}
