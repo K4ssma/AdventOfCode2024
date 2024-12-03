8b7401cc8ddc903345d6f5e983f4233f910b90f2
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 09:14:45 +0100

    added Unit-Test for "SimilarityCalculator"

---
+++ TestAdventOfCode2024/Day01/Task02/TSimilarityCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day01.Task02;
+
+using AdventOfCode2024.Day01.Task02;
+
+[TestFixture]
+public sealed class TSimilarityCalculator
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.SimilarityCases))]
+    public int CalculateSimilarityScore_ShouldReturnCorrectScore((int NumOne, int NumTwo)[] numbers)
+    {
+        // assert
+        return SimilarityCalculator.CalculateSimilarityScore(numbers);
+    }
+}
