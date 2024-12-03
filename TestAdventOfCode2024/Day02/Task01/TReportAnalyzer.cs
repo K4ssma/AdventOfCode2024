718aab2e3205c5dd205d51047bbb7b07f19806b5
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 11:01:25 +0100

    added Unit-Test for "Day02.Task01.ReportAnalyzer"

---
+++ TestAdventOfCode2024/Day02/Task01/TReportAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day02.Task01;
+
+using AdventOfCode2024.Day02.Task01;
+
+[TestFixture]
+public sealed class TReportAnalyzer
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ReportCases))]
+    public int GetNumOfSafereports_ShouldReturnCorrectNum(int[][] reports)
+    {
+        // assert
+        return ReportAnalyzer.GetNumOfSafeReports(reports);
+    }
+}
