079cd3250c42d2a24ced6a10345239fac1edf80f
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 09:14:45 +0100

    added Unit-Test for "SimilarityCalculator"

---
+++ TestAdventOfCode2024/Day01/Task02/TestCases.cs
@@ -0,0 +1,34 @@
+﻿namespace TestAdventOfCode2024.Day01.Task02;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TestCases
+{
+    public static IEnumerable<TestCaseData> SimilarityCases
+    {
+        get
+        {
+            yield return new TestCaseData(new (int NumOne, int NumTwo)[]
+            {
+                new(1, 2),
+                new(2, 1),
+            }).Returns(3);
+
+            yield return new TestCaseData(new (int NumOne, int NumTwo)[]
+            {
+                new(1, 1),
+                new(2, 1),
+            }).Returns(2);
+
+            yield return new TestCaseData(new (int NumOne, int NumTwo)[]
+            {
+                new(1, 3),
+                new(2, 3),
+            }).Returns(0);
+        }
+    }
+}
