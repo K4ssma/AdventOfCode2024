ac8dbe3251e5aeb5751b73381ed602b7d26a7774
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 09:05:40 +0100

    added "SimilarityCalculator" class without functionality

---
+++ AdventOfCode2024/Day01/Task02/SimilarityCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day01.Task02;
+
+using System;
+
+public static class SimilarityCalculator
+{
+    public static int CalculateSimilarityScore((int NumOne, int NumTwo)[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+}
