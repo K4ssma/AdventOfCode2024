4d114e5bbbd0e4693808f2a136bfcb53c32ba771
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 11:34:49 +0100

    added functionalitiy to "Day02.Task01.ReportAnalyzer"

---
+++ AdventOfCode2024/Day02/Task01/ReportAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2024.Day02.Task01;
+
+using System;
+using System.Linq;
+
+public static class ReportAnalyzer
+{
+    public static int GetNumOfSafeReports(int[][] reports)
+    {
+        return reports
+            .Count(report =>
+            {
+                if (report.Length < 2)
+                {
+                    return true;
+                }
+
+                if (report[0] - report[1] == 0)
+                {
+                    return false;
+                }
+
+                bool isIncreasing = report[0] - report[1] < 0;
+
+                int lastLvl = report[0];
+
+                for (int i = 1; i < report.Length; i++)
+                {
+                    if ((report[i] - lastLvl <= 0 && isIncreasing)
+                        || (report[i] - lastLvl >= 0 && !isIncreasing)
+                        || Math.Abs(report[i] - lastLvl) > 3)
+                    {
+                        return false;
+                    }
+
+                    lastLvl = report[i];
+                }
+
+                return true;
+            });
+    }
+}
