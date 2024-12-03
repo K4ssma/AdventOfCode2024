916c369c06236c39f701404933eb1dd821a2abb1
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 15:10:24 +0100

    added "Day02.Task02.ProblemDampener" class without functionality

---
+++ AdventOfCode2024/Day02/Task02/ProblemDampener.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day02.Task02;
+
+using System;
+
+public static class ProblemDampener
+{
+    public static int GetNumOfSafeReports(int[][] reports)
+    {
+        throw new NotImplementedException();
+    }
+}
