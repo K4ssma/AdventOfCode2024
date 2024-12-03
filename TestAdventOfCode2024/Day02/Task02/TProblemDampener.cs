d3a491d7b282bc02af52ca6ff7e09d72ae130b51
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-03 15:17:50 +0100

    added Unit-Test for "Day02.Task02.ProblemDampener"

---
+++ TestAdventOfCode2024/Day02/Task02/TProblemDampener.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day02.Task02;
+
+using AdventOfCode2024.Day02.Task02;
+
+[TestFixture]
+public sealed class TProblemDampener
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.ReportCases))]
+    public int GetNumOfSafeReports_ShouldReturnCorrectReportCount(int[][] reports, object? _)
+    {
+        // assert
+        return ProblemDampener.GetNumOfSafeReports(reports);
+    }
+}
