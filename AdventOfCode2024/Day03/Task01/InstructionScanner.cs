64ec3a5089a774121d24d7565217d03b282fdca1
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 11:11:23 +0100

    added "Day03.Task01.InstructionScanner" class without functionality

---
+++ AdventOfCode2024/Day03/Task01/InstructionScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day03.Task01;
+
+using System;
+
+public static class InstructionScanner
+{
+    public static int GetInstructionResult(string instruction)
+    {
+        throw new NotImplementedException();
+    }
+}
