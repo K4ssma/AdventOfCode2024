06933a1840f1662c93dfc5d383275d80366ff40e
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 11:43:56 +0100

    added functionality to "Day03.Task01.InstructionScanner"

--- AdventOfCode2024/Day03/Task01/InstructionScanner.cs
+++ AdventOfCode2024/Day03/Task01/InstructionScanner.cs
@@ -1,11 +1,30 @@
 ﻿namespace AdventOfCode2024.Day03.Task01;
 
-using System;
+using System.Text.RegularExpressions;
 
 public static class InstructionScanner
 {
-    public static int GetInstructionResult(string instruction)
+    public static int GetInstructionResult(string instructionString)
     {
-        throw new NotImplementedException();
+        Regex compiledRegex = new(@"\d{1,3}", RegexOptions.Compiled);
+
+        MatchCollection matches = Regex.Matches(instructionString, @"mul\(\d{1,3},\d{1,3}\)");
+
+        if (matches.Count == 0)
+        {
+            return 0;
+        }
+
+        return matches
+            .Select(instruction =>
+            {
+                int[] numbers = compiledRegex
+                    .Matches(instruction.Value)
+                    .Select(numMatch => int.Parse(numMatch.Value))
+                    .ToArray();
+
+                return numbers[0] * numbers[1];
+            })
+            .Aggregate((curr, next) => curr + next);
     }
 }
