4b4a7742e5788b3b7a2e572a05fa5a0aa2743c23
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 11:22:28 +0100

    added Unit-Test for "Day03.Task01.InstructionScanner"

---
+++ TestAdventOfCode2024/Day03/Task01/TInstructionScanner.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day03.Task01;
+
+using AdventOfCode2024.Day03.Task01;
+
+[TestFixture]
+public sealed class TInstructionScanner
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.InstructionCases))]
+    public int GetInstructionResult_ValidInstructions_ShouldReturnCorrectResult(string instruction)
+    {
+        // assert
+        return InstructionScanner.GetInstructionResult(instruction);
+    }
+}
---
+++ TestAdventOfCode2024/Day03/Task01/TestCases.cs
@@ -0,0 +1,29 @@
+﻿namespace TestAdventOfCode2024.Day03.Task01;
+
+using System.Collections.Generic;
+
+public static class TestCases
+{
+    public static IEnumerable<TestCaseData> InstructionCases
+    {
+        get
+        {
+            yield return new TestCaseData("mul(44,46)").Returns(44 * 46).SetName("Test 01");
+
+            yield return new TestCaseData("mul(123,4)").Returns(123 * 4).SetName("Test 02");
+
+            yield return new TestCaseData("mul(4*").Returns(0).SetName("Test 03");
+
+            yield return new TestCaseData("mul(6,9!").Returns(0).SetName("Test 04");
+
+            yield return new TestCaseData("?(12,34)").Returns(0).SetName("Test 05");
+
+            yield return new TestCaseData("mul ( 2 , 4 )").Returns(0).SetName("Test 06");
+
+            yield return new TestCaseData(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+                .Returns((2 * 4) + (5 * 5) + (11 * 8) + (8 * 5))
+                .SetName("Complex Test");
+        }
+    }
+}
