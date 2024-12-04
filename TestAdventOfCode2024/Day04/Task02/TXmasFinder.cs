32ad71384fd47e0900066ff7bd407c6209af233b
Kassma <doerfer.cedric@gmail.com>
2024-12-04 20:37:53 +0100

    added Unit-Test for "Day04.Task02.XmasFinder"

---
+++ TestAdventOfCode2024/Day04/Task02/TXmasFinder.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day04.Task02;
+
+using AdventOfCode2024.Day04.Task02;
+
+[TestFixture]
+public sealed class TXmasFinder
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.XmasCases))]
+    public int GetXmasCount_ShouldReturnCorrectCount(string searchField)
+    {
+        // assert
+        return XmasFinder.GetXMasCount(searchField);
+    }
+}
---
+++ TestAdventOfCode2024/Day04/Task02/TestCases.cs
@@ -0,0 +1,48 @@
+﻿namespace TestAdventOfCode2024.Day04.Task02;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TestCases
+{
+    public static IEnumerable<TestCaseData> XmasCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                "M.M\r\n" +
+                ".A.\r\n" +
+                "S.S").Returns(1).SetName("TestCase 01");
+
+            yield return new TestCaseData(
+                "M.S\r\n" +
+                ".A.\r\n" +
+                "M.S").Returns(1).SetName("TestCase 02");
+
+            yield return new TestCaseData(
+                "S.M\r\n" +
+                ".A.\r\n" +
+                "S.M").Returns(1).SetName("TestCase 03");
+
+            yield return new TestCaseData(
+                "S.S\r\n" +
+                ".A.\r\n" +
+                "M.M").Returns(1).SetName("TestCase 04");
+
+            yield return new TestCaseData(
+                ".M.S......\r\n" +
+                "..A..MSMS.\r\n" +
+                ".M.S.MAA..\r\n" +
+                "..A.ASMSM.\r\n" +
+                ".M.S.M....\r\n" +
+                "..........\r\n" +
+                "S.S.S.S.S.\r\n" +
+                ".A.A.A.A..\r\n" +
+                "M.M.M.M.M.\r\n" +
+                "..........").Returns(9).SetName("Complex-Example");
+        }
+    }
+}
