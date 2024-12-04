f741fbb8ad513db55df545986857e83f32b7ad67
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 10:36:15 +0100

    modified test cases to better diferentiate them

--- TestAdventOfCode2024/Day02/Task02/TestCases.cs
+++ TestAdventOfCode2024/Day02/Task02/TestCases.cs
@@ -12,70 +12,44 @@ public static class TestCases
             yield return new TestCaseData(
                 new int[][]
                 {
-                    [1],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [1, 2, 3],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [3, 2, 1],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [1, 2, 1],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [1, 4],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [1, 5],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [4, 1],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
-                    [5, 0],
-                },
-                null).Returns(1);
-
-            yield return new TestCaseData(
-                new int[][]
-                {
                     [7, 6, 4, 2, 1],
+                },
+                null).Returns(1).SetName("AdventOfCode Example 01");
+
+            yield return new TestCaseData(
+                new int[][]
+                {
                     [1, 2, 7, 8, 9],
+                },
+                null).Returns(0).SetName("AdventOfCode Example 02");
+
+            yield return new TestCaseData(
+                new int[][]
+                {
                     [9, 7, 6, 2, 1],
+                },
+                null).Returns(0).SetName("AdventOfCode Example 03");
+
+            yield return new TestCaseData(
+                new int[][]
+                {
                     [1, 3, 2, 4, 5],
+                },
+                null).Returns(1).SetName("AdventOfCode Example 04");
+
+            yield return new TestCaseData(
+                new int[][]
+                {
                     [8, 6, 4, 4, 1],
+                },
+                null).Returns(1).SetName("AdventOfCode Example 05");
+
+            yield return new TestCaseData(
+                new int[][]
+                {
                     [1, 3, 6, 7, 9],
                 },
-                null).Returns(4);
+                null).Returns(1).SetName("AdventOfCode Example 06");
         }
     }
 }
