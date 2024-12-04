d2fd3bd0414d89b21ce04b12c1d87d9a7f7758c4
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 16:13:19 +0100

    added functionality to "Day03.Task03.ConditionalInstructionScanner"

--- AdventOfCode2024/Day03/Task02/ConditionalInstructionScanner.cs
+++ AdventOfCode2024/Day03/Task02/ConditionalInstructionScanner.cs
@@ -1,11 +1,43 @@
 ﻿namespace AdventOfCode2024.Day03.Task02;
 
-using System;
+using System.Text.RegularExpressions;
 
 public static class ConditionalInstructionScanner
 {
     public static int GetInstructionResult(string instruction)
     {
-        throw new NotImplementedException();
+        MatchCollection validInstructions = Regex
+            .Matches(instruction, @"(?<do>do\(\))|(?<dont>don't\(\))|mul\(\d{1,3},\d{1,3}\)");
+
+        Regex compiledNumRegex = new(@"\d{1,3}", RegexOptions.Compiled);
+
+        bool isActive = true;
+        int result = 0;
+
+        foreach (Match validInstruction in validInstructions)
+        {
+            if (validInstruction.Groups["do"].Success)
+            {
+                isActive = true;
+                continue;
+            }
+            else if (validInstruction.Groups["dont"].Success)
+            {
+                isActive = false;
+                continue;
+            }
+
+            if (!isActive)
+            {
+                continue;
+            }
+
+            result += compiledNumRegex
+                    .Matches(validInstruction.Value)
+                    .Select(numMatch => int.Parse(numMatch.Value))
+                    .Aggregate((curr, next) => curr * next);
+        }
+
+        return result;
     }
 }
