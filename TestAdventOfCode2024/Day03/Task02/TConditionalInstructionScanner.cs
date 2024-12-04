76fa8e0e38640c3777ca30321575a1352ca48d01
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 15:25:36 +0100

    adde Unit-Test for "Day03.Task02.ConditionalInstructionScanner"

---
+++ TestAdventOfCode2024/Day03/Task02/TConditionalInstructionScanner.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day03.Task02;
+
+using AdventOfCode2024.Day03.Task02;
+
+[TestFixture]
+public sealed class TConditionalInstructionScanner
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.InstructionCases))]
+    public int GetInstructionResult_ShouldReturnCorrectResult(string instruction)
+    {
+        // assert
+        return ConditionalInstructionScanner.GetInstructionResult(instruction);
+    }
+}
