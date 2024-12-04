044a51d2f38974d3e6c5e275e9c30524cddf7c77
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-04 15:16:37 +0100

    added "Day03.Task02.ConditionalInstructionScanner" without functionality

---
+++ AdventOfCode2024/Day03/Task02/ConditionalInstructionScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day03.Task02;
+
+using System;
+
+public static class ConditionalInstructionScanner
+{
+    public static int GetInstructionResult(string instruction)
+    {
+        throw new NotImplementedException();
+    }
+}
