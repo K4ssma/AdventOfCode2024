75a3a61205fe4f5d6a8db4bac05a59c71d4466cf
Kassma <doerfer.cedric@gmail.com>
2024-12-04 18:35:26 +0100

    added "Day04.Task01.WordSearcher" without functionality

---
+++ AdventOfCode2024/Day04/Task01/WordSearcher.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day04.Task01;
+
+using System;
+
+public static class WordSearcher
+{
+    public static int GetXmasCount(string wordFiels)
+    {
+        throw new NotImplementedException();
+    }
+}
