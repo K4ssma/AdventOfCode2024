2a254402ef7048b4b292634a8e2768bc22a8fc42
Kassma <doerfer.cedric@gmail.com>
2024-12-04 20:27:50 +0100

    added "Day04.Task02.XmasFinder" without functionality

---
+++ AdventOfCode2024/Day04/Task02/XmasFinder.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day04.Task02;
+
+using System;
+
+public static class XmasFinder
+{
+    public static int GetXMasCount(string searchField)
+    {
+        throw new NotImplementedException();
+    }
+}
