6ef4d0db973b0cd2a32f82e790db4a640baae0cf
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-05 09:06:35 +0100

    added "Day05.Task02.PrintCorrector" without functionality

---
+++ AdventOfCode2024/Day05/Task02/PrintCorrector.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day05.Task02;
+
+using System;
+
+public static class PrintCorrector
+{
+    public static int GetCorrectedMiddleSum(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
