effcdfed84a3ad62c5ef1973a1ac504af73e700f
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-05 09:11:41 +0100

    added Unit-Test for "Day05.Task02.PrintCorrector"

---
+++ TestAdventOfCode2024/Day05/Task02/TPrintCorrector.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day05.Task02;
+
+using AdventOfCode2024.Day05.Task02;
+
+[TestFixture]
+public sealed class TPrintCorrector
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.PrintPageCases))]
+    public int GetCorrectedMiddleSum_ShouldReturnCorrectSum(string input)
+    {
+        // assert
+        return PrintCorrector.GetCorrectedMiddleSum(input);
+    }
+}
