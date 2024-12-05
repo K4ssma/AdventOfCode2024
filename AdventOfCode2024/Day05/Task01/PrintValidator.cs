e006c349b71c27bf9c9215490e49a5a64fc65b71
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-05 07:46:30 +0100

    added "Day05.Task01.PrintValidator" class without functionality

---
+++ AdventOfCode2024/Day05/Task01/PrintValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day05.Task01;
+
+using System;
+
+public static class PrintValidator
+{
+    public static int GetValidMiddleSum(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
