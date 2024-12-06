6db3f99d419a86f322e0493f364fb010fb07d251
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-06 07:47:34 +0100

    added "Da06.Taks01.GuardPredictor" without functionality

---
+++ AdventOfCode2024/Day06/Task01/GuardPredictor.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Day06.Task01;
+
+using System;
+
+public static class GuardPredictor
+{
+    public static int PredictGuardLocations(string map)
+    {
+        throw new NotImplementedException();
+    }
+}
