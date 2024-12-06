f0f1f5ee68ece4724decf5b36a8521f8462ad4ca
Cedric Doerfer <cedric.doerfer@dako.de>
2024-12-06 10:38:48 +0100

    added Unit-Test for "Day06.MapReader"

---
+++ TestAdventOfCode2024/Day06/TMapReader.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAdventOfCode2024.Day06;
+
+using AdventOfCode2024.Day06.HelperClasses;
+
+[TestFixture]
+public sealed class TMapReader
+{
+    [TestCaseSource(typeof(TestCases), nameof(TestCases.MapStringCases))]
+    public (bool?[,] Map, GuardState GuardState) ReadMapString(string mapString)
+    {
+        // assert
+        return MapReader.ReadMapString(mapString);
+    }
+}
---
+++ TestAdventOfCode2024/Day06/TestCases.cs
@@ -0,0 +1,118 @@
+﻿namespace TestAdventOfCode2024.Day06;
+
+using System.Collections.Generic;
+using AdventOfCode2024.Day06.HelperClasses;
+
+public static class TestCases
+{
+    public static IEnumerable<TestCaseData> MapStringCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                ".>...#..")
+                .Returns((new bool?[,]
+                {
+                    { false },
+                    { false },
+                    { false },
+                    { false },
+                    { false },
+                    { null },
+                    { false },
+                    { false },
+                }, new GuardState
+                {
+                    Pos = new() { X = 1, Y = 0 },
+                    Direction = Direction.East,
+                }))
+                .SetName("TestCase 01");
+
+            yield return new TestCaseData(
+                ".#...<.")
+                .Returns((new bool?[,]
+                {
+                    { false },
+                    { null },
+                    { false },
+                    { false },
+                    { false },
+                    { false },
+                    { false },
+                }, new GuardState
+                {
+                    Pos = new() { X = 5, Y = 0 },
+                    Direction = Direction.West,
+                }))
+                .SetName("TestCase 02");
+
+            yield return new TestCaseData(
+                "..\r\n" +
+                ".#\r\n" +
+                "..\r\n" +
+                "..\r\n" +
+                "..\r\n" +
+                ".^\r\n" +
+                "..")
+                .Returns((new bool?[,]
+                {
+                    { false, false, false, false, false, false, false },
+                    { false, null, false, false, false, false, false },
+                }, new GuardState()
+                {
+                    Pos = new() { X = 1, Y = 5 },
+                    Direction = Direction.North,
+                }))
+                .SetName("TestCase 03");
+
+            yield return new TestCaseData(
+                "..\r\n" +
+                "v.\r\n" +
+                "..\r\n" +
+                "..\r\n" +
+                "..\r\n" +
+                "#.\r\n" +
+                "..")
+                .Returns((new bool?[,]
+                {
+                    { false, false, false, false, false, null, false },
+                    { false, false, false, false, false, false, false },
+                }, new GuardState()
+                {
+                    Pos = new() { X = 0, Y = 1 },
+                    Direction = Direction.South,
+                }))
+                .SetName("TestCase 04");
+
+            yield return new TestCaseData(
+                "....#.....\r\n" +
+                ".........#\r\n" +
+                "..........\r\n" +
+                "..#.......\r\n" +
+                ".......#..\r\n" +
+                "..........\r\n" +
+                ".#..^.....\r\n" +
+                "........#.\r\n" +
+                "#.........\r\n" +
+                "......#...")
+                .Returns((new bool?[,]
+                {
+                    { false, false, false, false, false, false, false, false, null, false },
+                    { false, false, false, false, false, false, null, false, false, false },
+                    { false, false, false, null, false, false, false, false, false, false },
+                    { false, false, false, false, false, false, false, false, false, false },
+                    { null, false, false, false, false, false, false, false, false, false },
+                    { false, false, false, false, false, false, false, false, false, false },
+                    { false, false, false, false, false, false, false, false, false, null },
+                    { false, false, false, false, null, false, false, false, false, false },
+                    { false, false, false, false, false, false, false, null, false, false },
+                    { false, null, false, false, false, false, false, false, false, false },
+                }, new GuardState()
+                {
+                    Pos = new() { X = 4, Y = 6 },
+                    Direction = Direction.North,
+                }))
+                .SetName("Complex Example");
+        }
+    }
+}
