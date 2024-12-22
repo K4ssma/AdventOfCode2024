e27a24864de45b05060d4b4986510371cd01455f
Kassma <doerfer.cedric@gmail.com>
2024-12-22 10:02:39 +0100

    added Unit-Test for "Day06.Task01.GuardPredictor"

---
+++ TestAdventOfCode2024/Day06/Task01/TGuardPredictor.cs
@@ -0,0 +1,30 @@
+﻿namespace TestAdventOfCode2024.Day06.Task01;
+
+using AdventOfCode2024.Day06.Task01;
+
+[TestFixture]
+public sealed class TGuardPredictor
+{
+    [Test]
+    public void PredictGuardPositions_ShouldReturnCorrectDistinctGuardPositions()
+    {
+        // arrange
+        string mapString =
+            "....#.....\r\n" +
+            ".........#\r\n" +
+            "..........\r\n" +
+            "..#.......\r\n" +
+            ".......#..\r\n" +
+            "..........\r\n" +
+            ".#..^.....\r\n" +
+            "........#.\r\n" +
+            "#.........\r\n" +
+            "......#...";
+
+        // act
+        int posCount = GuardPredictor.PredictGuardPositions(mapString);
+
+        // assert
+        Assert.That(posCount, Is.EqualTo(41));
+    }
+}
