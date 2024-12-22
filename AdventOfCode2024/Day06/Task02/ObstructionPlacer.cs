0aac3e9ccd1fcdb30d60e8341de3e6f918faa1d6
Kassma <doerfer.cedric@gmail.com>
2024-12-22 17:41:39 +0100

    added Day06 Task02

---
+++ AdventOfCode2024/Day06/Task02/ObstructionPlacer.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2024.Day06.Task02;
+
+using AdventOfCode2024.Day06.HelperClasses;
+
+public static class ObstructionPlacer
+{
+    public static int GetPossibleObstructionCount(string mapString)
+    {
+        FlagableMap map = new(mapString);
+
+        int possibleObstructionCount = 0;
+
+        do
+        {
+            map.MarkCurrPosIfEmpty();
+
+            map.SaveState();
+
+            if (!map.PlaceObstruction())
+            {
+                break;
+            }
+
+            while (map.MoveNext())
+            {
+                if (map.IsOnLoop())
+                {
+                    possibleObstructionCount++;
+                    break;
+                }
+
+                map.MarkCurrPosIfEmpty();
+            }
+
+            map.LoadSavedState();
+        }
+        while (map.MoveNext());
+
+        return possibleObstructionCount;
+    }
+}
