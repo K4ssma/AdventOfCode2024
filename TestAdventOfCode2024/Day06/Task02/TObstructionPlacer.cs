ba21988f6b9a96f5dc54928234209abe306324ff
Kassma <doerfer.cedric@gmail.com>
2024-12-22 17:54:18 +0100

    added Unit-Test for "Day05.Task02.ObstructionPlacer"

---
+++ TestAdventOfCode2024/Day06/Task02/TObstructionPlacer.cs
@@ -0,0 +1,30 @@
+﻿namespace TestAdventOfCode2024.Day06.Task02;
+
+using AdventOfCode2024.Day06.Task02;
+
+[TestFixture]
+public sealed class TObstructionPlacer
+{
+    [Test]
+    public void GetPossibleObstructionCount_ShouldReturnCorrectObstructionAmount()
+    {
+        // arrange
+        string mapString =
+            "....#.....\r\n" +
+            ".........#\r\n" +
+            "..........\r\n" +
+            "..#.......\r\n" +
+            ".......#..\r\n" +
+            "..........\r\n" +
+            ".#..^.....\r\n" +
+            "........#.\r\n" +
+            "#.........\r\n" +
+            "......#...";
+
+        // act
+        int possibleObstructionCount = ObstructionPlacer.GetPossibleObstructionCount(mapString);
+
+        // assert
+        Assert.That(possibleObstructionCount, Is.EqualTo(6));
+    }
+}
