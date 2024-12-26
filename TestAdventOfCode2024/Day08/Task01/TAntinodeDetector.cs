8085f98bcac357feb95ef54c1faa0dd062fe81de
Kassma <doerfer.cedric@gmail.com>
2024-12-27 00:54:08 +0100

    Day08: added Unit-Test for "AntinodeDetector"

---
+++ TestAdventOfCode2024/Day08/Task01/TAntinodeDetector.cs
@@ -0,0 +1,32 @@
+﻿namespace TestAdventOfCode2024.Day08.Task01;
+
+using AdventOfCode2024.Day08.Task01;
+
+[TestFixture]
+public sealed class TAntinodeDetector
+{
+    [Test]
+    public void CountUniqueAntinodeSpots_ShouldReturnCorrectAntinodeCount()
+    {
+        // arrange
+        string inputString =
+            "............\r\n" +
+            "........0...\r\n" +
+            ".....0......\r\n" +
+            ".......0....\r\n" +
+            "....0.......\r\n" +
+            "......A.....\r\n" +
+            "............\r\n" +
+            "............\r\n" +
+            "........A...\r\n" +
+            ".........A..\r\n" +
+            "............\r\n" +
+            "............";
+
+        // act
+        int antinodeCount = AntinodeDetector.CountUniqueAntinodeSpots(inputString);
+
+        // assert
+        Assert.That(antinodeCount, Is.EqualTo(14));
+    }
+}
