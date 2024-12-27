0a241401c291f53749c6fae0245acd4530dbb5cc
Kassma <doerfer.cedric@gmail.com>
2024-12-27 21:39:38 +0100

    Day08: added Unit-Test for "CorrectedNodeDetector"

---
+++ TestAdventOfCode2024/Day08/Task02/TCorrectedNodeDetector.cs
@@ -0,0 +1,37 @@
+﻿namespace TestAdventOfCode2024.Day08.Task02;
+
+using System.Collections.Generic;
+using AdventOfCode2024.Day08;
+using AdventOfCode2024.Day08.Task02;
+using AdventOfCode2024.HelperClasses;
+
+[TestFixture]
+public sealed class TCorrectedNodeDetector
+{
+    [Test]
+    public void CountUniqueAntinodeSpots_ShouldReturnCorrectAntinodeCount()
+    {
+        // arrange
+        string inputString =
+            "............\r\n" +
+            "........0...\r\n" +
+            ".....0......\r\n" +
+            ".......0....\r\n" +
+            "....0.......\r\n" +
+            "......A.....\r\n" +
+            "............\r\n" +
+            "............\r\n" +
+            "........A...\r\n" +
+            ".........A..\r\n" +
+            "............\r\n" +
+            "............";
+
+        // act
+        (Vector2Int, Dictionary<char, List<Vector2Int>>) readInput = InputReader.ReadInputString(inputString);
+        int antinodeCount = CorrectedNodeDetector.GetNodeCount(
+            readInput.Item1, readInput.Item2);
+
+        // assert
+        Assert.That(antinodeCount, Is.EqualTo(34));
+    }
+}
