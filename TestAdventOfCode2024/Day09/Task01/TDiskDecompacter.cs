e2a1dc4d08db962fcd4b385128e5d543caac00f3
Kassma <doerfer.cedric@gmail.com>
2024-12-28 00:13:44 +0100

    Day09: added Unit-Test for "DiskDecompacter"

---
+++ TestAdventOfCode2024/Day09/Task01/TDiskDecompacter.cs
@@ -0,0 +1,20 @@
+﻿namespace TestAdventOfCode2024.Day09.Task01;
+
+using AdventOfCode2024.Day09.Task01;
+
+[TestFixture]
+public sealed class TDiskDecompacter
+{
+    [Test]
+    public void GetChecksum()
+    {
+        // arrange
+        string inputString = "2333133121414131402";
+
+        // act
+        int checkSum = DiskDecompacter.GetChecksum(inputString);
+
+        // assert
+        Assert.That(checkSum, Is.EqualTo(1928));
+    }
+}
